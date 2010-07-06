fb632ecbfbba1edd8692d4c6f9b1ff3e33b56fed
Albert Volkman <albert@27squared.com>
2010-07-06 13:50:50 -0400

    Separated class into separate file

--- mean.php
+++ mean.php
@@ -1,58 +1,6 @@
 <?php
 
-class rangeFinder {
-	public $datapoints = array();
-	public $range = array();
-	public $final = array();
-
-	function __construct($datapoints) {
-		$this->datapoints = $datapoints;
-	}
-
-	function find() {
-		$this->data_range();
-		$this->smaller_range();
-	}
-
-	function data_range () {
-		$range = array();
-		
-		$count = count($this->datapoints);
-		
-		for ($i=1; $i < $count; $i++) {
-			$x = $this->datapoints[$i] - $this->datapoints[$i-1];
-			if ($x < 0) {
-				$x = $x - ($x * 2);
-			}
-		
-			$this->range[] = $x;
-		}
-	}
-	
-	function smaller_range() {
-		$prior = '';
-		foreach ($this->range as $key=>$data) {
-			if($prior && $prior <= $data) {
-				echo "x = [" . implode(' ', $this->datapoints) . "]\n";
-				echo "r = [" . implode(' ', $this->range) . "]\n";
-				echo "Deleting: " . $this->datapoints[$key] . " & " . $this->datapoints[$key - 1] . "\n";
-				$item['range'] = $prior;
-				$item['mean'] = ($this->datapoints[$key] - $this->datapoints[$key - 1]) / 2;
-				echo "Range: " . $prior . "\n";
-				echo "Mean: " . $item['mean'] . "\n\n";
-				$this->final[] = $item;
-				unset($this->datapoints[$key]);
-				unset($this->datapoints[$key - 1]);
-				$this->datapoints = array_values($this->datapoints);
-				break;
-			}
-		
-			$prior = $data;
-		}
-
-		$this->range = array();
-	}
-}
+include('rangeFinder.php');
 
 $datapoints = array(80,20,60,-100,40,-20,0,-80,80,-40,80);
 $range_finder = new rangeFinder($datapoints);
